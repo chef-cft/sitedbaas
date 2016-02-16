2126363af3450225a05707d4b0719f70766f94ac
Chef Sales Engineering <saleseng@chef.io>
2016-02-16 20:54:31 +0000

    fixing default recipe to manage apache2.conf and updating audit recipe to audit apache2.conf

--- recipes/ubuntu-audit-apache.rb
+++ recipes/ubuntu-audit-apache.rb
@@ -7,7 +7,7 @@
 control_group 'Validate apache configuration' do
   control 'Ensure directory listing is not allowed' do
     it 'does not allow directory listing' do
-      expect(file('/etc/apache2/httpd.conf').content).to_not match /Options Indexes/
+      expect(file('/etc/apache2/apache2.conf').content).to_not match /Options Indexes/
     end
   end
 end
